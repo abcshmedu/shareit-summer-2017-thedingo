5641caa2c0cf2b3c540acb6cfb34f93cf06e9182
Faaux <c.brammer1@gmail.com>
2017-04-12 13:59:13 +0200

    Added Book

--- src/main/java/edu/hm/cbrammer/swa/models/Book.java
+++ src/main/java/edu/hm/cbrammer/swa/models/Book.java
@@ -1,8 +1,53 @@
 package edu.hm.cbrammer.swa.models;
 
-/**
- * Created by cbram on 12.04.2017.
- */
-public class Book
+public class Book extends Medium
 {
+    private final String author;
+    private final String isbn;
+
+    public Book(String title, String author, String isbn)
+    {
+        super(title);
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    private Book()
+    {
+        super("");
+        this.author = "";
+        this.isbn = "";
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Book book = (Book) o;
+
+        if (!getAuthor().equals(book.getAuthor())) return false;
+        return getIsbn().equals(book.getIsbn());
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }

7a6835836933663057d947d8bb0d77e65e4f770b
Thomas Stachl <stachl@hm.edu>
2017-04-12 14:56:07 +0200

    create book added

--- src/main/java/edu/hm/cbrammer/stachl/swa/controller/MediaResource.java
+++ src/main/java/edu/hm/cbrammer/stachl/swa/controller/MediaResource.java
@@ -1,8 +1,13 @@
 package edu.hm.cbrammer.stachl.swa.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.hm.cbrammer.stachl.swa.models.Book;
+import org.json.JSONObject;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.xml.ws.Response;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 /**
  * Created by stach on 12-Apr-17.
@@ -10,11 +15,35 @@ import javax.xml.ws.Response;
 @Path("/media")
 public class MediaResource {
 
+    private final MediaService mediaService;
+
+    public MediaResource(MediaService mediaService){
+        this.mediaService = mediaService;
+    }
+
     @POST
     @Path("/books")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createBook(){
+    public Response createBook(String json){
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        Response response;
+        try{
+            Book book = objectMapper.readValue(json,Book.class);
+            mediaService.addBook(book);
+            //TODO Header Esponses from add Book
+            response = null;
+        }
+        catch(IOException e){
+            JSONObject object = new JSONObject();
+            object.append("code", Response.Status.BAD_REQUEST.getStatusCode());
+            object.append("detail", "Error: Json not parsable");
+            response = Response.status(Response.Status.BAD_REQUEST)
+                    .entity(object.toString())
+                    .build();
+        }
+        return response;
     }
 
     @GET
@@ -31,6 +60,7 @@ public class MediaResource {
 
     @PUT
     @Path("/books/{isbn}")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBook(@PathParam("isbn") String isbn ){
 
@@ -42,6 +72,7 @@ public class MediaResource {
 
     @POST
     @Path("/discs")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createDisc(){
 
@@ -62,6 +93,7 @@ public class MediaResource {
     @PUT
     @Path("/discs/{barcode}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response getDisc(@PathParam("barcode") String barcode ){
 
     }

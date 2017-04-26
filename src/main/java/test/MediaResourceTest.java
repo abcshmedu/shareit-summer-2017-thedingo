0136f62cac5b6860df91e9cc1378bb3d024dfa40
Thomas Stachl <stachl@hm.edu>
2017-04-26 14:50:19 +0200

    finished the tests

--- src/main/java/test/MediaResourceTest.java
+++ src/main/java/test/MediaResourceTest.java
@@ -5,6 +5,7 @@ import edu.hm.cbrammer.stachl.swa.controller.MediaService;
 import edu.hm.cbrammer.stachl.swa.controller.MediaServiceImpl;
 import edu.hm.cbrammer.stachl.swa.controller.MediaServiceResult;
 import edu.hm.cbrammer.stachl.swa.models.Book;
+import edu.hm.cbrammer.stachl.swa.models.Disc;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
@@ -84,7 +85,7 @@ public class MediaResourceTest {
     }
 
     @Test
-    public void updateBookAISBN() throws Exception {
+    public void updateBookISBN() throws Exception {
         Book testBook = new Book("T", "I", "78965");
         mediaResource.createBook(testBook);
         Response response = mediaResource.updateBook(new Book("","", "test"),testBook.getIsbn());
@@ -103,24 +104,115 @@ public class MediaResourceTest {
         assertEquals(response.getStatus(),Response.Status.NOT_FOUND.getStatusCode());
 
     }
+
     @Test
     public void createDisc() throws Exception {
 
+        Response result = mediaResource.createDisc(new Disc("A","kkkkk", "C",12));
+        assertEquals(result.getStatus(),Response.Status.CREATED.getStatusCode());
+
     }
+
     @Test
     public void createExistingDisc() throws Exception {
+
+        mediaResource.createDisc(new Disc("A","B", "C",12));
+        Response result = mediaResource.createDisc(new Disc("A","B", "C",12));
+        assertEquals(result.getStatus(),Response.Status.BAD_REQUEST.getStatusCode());
     }
 
     @Test
     public void getDisc() throws Exception {
+        Disc testDisc = new Disc("C", "D", "author", 12);
+        mediaResource.createDisc(testDisc);
+        Disc disc = mediaResource.getDisc("D");
+        assertEquals(disc,testDisc);
+
     }
 
     @Test
     public void getDiscs() throws Exception {
+
+        Disc testDisc = new Disc("E", "F",  "james",16);
+        Disc testDisc2 = new Disc("G", "H",  "hannes", 18);
+        mediaResource.createDisc(testDisc);
+        mediaResource.createDisc(testDisc2);
+
+        Disc[] discs = mediaResource.getDiscs();
+
+        boolean disc1Found = false;
+        boolean disc2Found = false;
+        for(Disc disc: discs)
+        {
+            if(disc.equals(testDisc))
+                disc1Found = true;
+            if(disc.equals(testDisc2))
+                disc2Found = true;
+        }
+
+        assertTrue(disc1Found && disc2Found);
     }
 
     @Test
-    public void updateDisc() throws Exception {
+    public void updateDiscTitle() throws Exception {
+        Disc testDisc = new Disc("T", "I", "inge",15);
+        mediaResource.createDisc(testDisc);
+        Response response = mediaResource.updateDisc(new Disc("Hallo","", "",-1),testDisc.getBarcode());
+        assertEquals(response.getStatus(),Response.Status.OK.getStatusCode());
+        Disc result = mediaResource.getDisc(testDisc.getBarcode());
+        assertEquals(result.getTitle(), "Hallo");
+        assertEquals(result.getDirector(), testDisc.getDirector());
+        assertEquals(result.getFsk(), testDisc.getFsk());
+    }
+
+    @Test
+    public void updateDiscDirector() throws Exception {
+
+        Disc testDisc = new Disc("h", "df", "inge",15);
+        mediaResource.createDisc(testDisc);
+        Response response = mediaResource.updateDisc(new Disc("","", "jürgen",-1),testDisc.getBarcode());
+        assertEquals(response.getStatus(),Response.Status.OK.getStatusCode());
+        Disc result = mediaResource.getDisc(testDisc.getBarcode());
+        assertEquals(result.getTitle(), testDisc.getTitle());
+        assertEquals(result.getDirector(), "jürgen");
+        assertEquals(result.getFsk(), testDisc.getFsk());
+    }
+
+    @Test
+    public void updateDiscFSK() throws Exception {
+
+        Disc testDisc = new Disc("T", "zuuu", "inge",15);
+        mediaResource.createDisc(testDisc);
+        Response response = mediaResource.updateDisc(new Disc("","", "",7),testDisc.getBarcode());
+        assertEquals(response.getStatus(),Response.Status.OK.getStatusCode());
+        Disc result = mediaResource.getDisc(testDisc.getBarcode());
+        assertEquals(result.getTitle(), testDisc.getTitle());
+        assertEquals(result.getDirector(), testDisc.getDirector());
+        assertEquals(result.getFsk(), 7);
+    }
+
+    @Test
+    public void updateDiscBarcode() throws Exception {
+
+        Disc testDisc = new Disc("T", "I", "inge",15);
+        mediaResource.createDisc(testDisc);
+        Response response = mediaResource.updateDisc(new Disc("","haaaaa", "",-1),testDisc.getBarcode());
+        assertEquals(response.getStatus(),Response.Status.OK.getStatusCode());
+        Disc result = mediaResource.getDisc(testDisc.getBarcode());
+        assertEquals(result.getTitle(), testDisc.getTitle());
+        assertEquals(result.getDirector(), testDisc.getDirector());
+        assertEquals(result.getFsk(), testDisc.getFsk());
+        result = mediaResource.getDisc("haaaaa");
+        assertEquals(result,null);
+    }
+
+    @Test
+    public void updateDiscNotFound() throws Exception {
+
+        Response response = mediaResource.updateDisc(new Disc("","", "",-1),"i don't exist");
+        assertEquals(response.getStatus(),Response.Status.NOT_FOUND.getStatusCode());
+
+
     }
 
 }

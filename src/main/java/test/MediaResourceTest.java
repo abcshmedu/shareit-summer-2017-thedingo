4a727f95e5fa470123c2f6fa0c4c304117b294e1
Thomas Stachl <stachl@hm.edu>
2017-04-26 14:20:00 +0200

    started testing

---
+++ src/main/java/test/MediaResourceTest.java
@@ -0,0 +1,94 @@
+package test;
+
+import edu.hm.cbrammer.stachl.swa.controller.MediaResource;
+import edu.hm.cbrammer.stachl.swa.controller.MediaService;
+import edu.hm.cbrammer.stachl.swa.controller.MediaServiceImpl;
+import edu.hm.cbrammer.stachl.swa.controller.MediaServiceResult;
+import edu.hm.cbrammer.stachl.swa.models.Book;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+
+
+public class MediaResourceTest {
+
+    private static MediaResource mediaResource = new MediaResource();
+
+    @Test
+    public void createBook() throws Exception {
+
+        Response result = mediaResource.createBook(new Book("A","B", "1337"));
+        assertEquals(result.getStatus(),Response.Status.CREATED.getStatusCode());
+    }
+
+    @Test
+    public void createExistingBook() throws Exception {
+
+        mediaResource.createBook(new Book("A","B", "1338"));
+        Response result = mediaResource.createBook(new Book("A","B", "1338"));
+        assertEquals(result.getStatus(),Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+
+
+    @Test
+    public void getBook() throws Exception {
+        Book testBook = new Book("C", "D", "1234");
+        mediaResource.createBook(testBook);
+        Book book = mediaResource.getBook("1234");
+        assertEquals(book,testBook);
+    }
+
+    @Test
+    public void getBooks() throws Exception {
+        Book testBook = new Book("E", "F",  "5678");
+        Book testBook2 = new Book("G", "H",  "9123");
+        mediaResource.createBook(testBook);
+        mediaResource.createBook(testBook2);
+
+        Book[] books = mediaResource.getBooks();
+
+        boolean book1Found = false;
+        boolean book2Found = false;
+        for(Book book: books)
+        {
+            if(book.equals(testBook))
+                book1Found = true;
+            if(book.equals(testBook2))
+                book2Found = true;
+        }
+
+        assertTrue(book1Found && book2Found);
+    }
+
+    @Test
+    public void updateBook() throws Exception {
+        Book testBook = new Book("T", "I", "58686");
+        mediaResource.createBook(testBook);
+        Response response = mediaResource.updateBook(new Book("Hallo","", ""),testBook.getIsbn());
+        assertEquals(response,Response.Status.OK);
+    }
+
+    @Test
+    public void createDisc() throws Exception {
+
+    }
+    @Test
+    public void createExistingDisc() throws Exception {
+    }
+
+    @Test
+    public void getDisc() throws Exception {
+    }
+
+    @Test
+    public void getDiscs() throws Exception {
+    }
+
+    @Test
+    public void updateDisc() throws Exception {
+    }
+
+}

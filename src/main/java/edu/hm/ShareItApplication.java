e7a751695a6b581d9df182f778a2f0afd650dfe3
Thomas Stachl <stachl@hm.edu>
2017-05-31 13:53:54 +0200

    preparing new lab

---
+++ src/main/java/edu/hm/ShareItApplication.java
@@ -0,0 +1,22 @@
+package edu.hm;
+
+import javax.inject.Inject;
+
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
+import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
+
+/**
+ * Application class to enable guice within jersey.
+ */
+public class ShareItApplication extends ResourceConfig {
+    @Inject
+    public ShareItApplication(ServiceLocator serviceLocator) {
+        GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
+        GuiceIntoHK2Bridge guiceBridge
+                = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+        guiceBridge.bridgeGuiceInjector(
+                ShareitServletContextListener.getInjectorInstance());
+    }
+}
---
+++ src/main/java/edu/hm/ShareitServletContextListener.java
@@ -0,0 +1,7 @@
+package edu.hm;
+
+/**
+ * Created by stach on 31-May-17.
+ */
+public class ShareitServletContextListener {
+}

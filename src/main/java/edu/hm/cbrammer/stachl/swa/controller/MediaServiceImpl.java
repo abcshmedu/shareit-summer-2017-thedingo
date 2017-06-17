9538090104a74ced4909fda6fce26325f6f84c84
Chris Brammer <c.brammer1@gmail.com>
2017-06-17 17:23:14 +0200

    Reformat

--- src/main/java/edu/hm/cbrammer/stachl/swa/controller/MediaServiceImpl.java
+++ src/main/java/edu/hm/cbrammer/stachl/swa/controller/MediaServiceImpl.java
@@ -9,34 +9,26 @@ import edu.hm.cbrammer.stachl.swa.models.MediaPersistence;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 
-public class MediaServiceImpl implements MediaService
-{
+public class MediaServiceImpl implements MediaService {
     private final MediaPersistence mediaPersistence;
 
     @Inject
-    public MediaServiceImpl(MediaPersistence mediaPersistence)
-    {
+    public MediaServiceImpl(MediaPersistence mediaPersistence) {
         this.mediaPersistence = mediaPersistence;
     }
 
     @Override
-    public MediaServiceResult addBook(Book book)
-    {
+    public MediaServiceResult addBook(Book book) {
         MediaServiceResult result;
-        try
-        {
+        try {
             final String checkedIsbn = Isbn.of(book.getIsbn());
-            if (mediaPersistence.getBookIfExists(checkedIsbn) != null)
-            {
+            if (mediaPersistence.getBookIfExists(checkedIsbn) != null) {
                 result = new MediaServiceResult(Response.Status.BAD_REQUEST, String.format("Book with ISBN '%s' already exists.", book.getIsbn()));
-            }
-            else
-            {
-                mediaPersistence.updateOrCreate(book);
+            } else {
+                mediaPersistence.updateOrCreate(new Book(book.getTitle(), book.getAuthor(), checkedIsbn));
                 result = new MediaServiceResult(Response.Status.CREATED, String.format("Book with ISBN '%s' was successfully created.", book.getIsbn()));
             }
-        } catch (IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
             result = new MediaServiceResult(Response.Status.BAD_REQUEST, String.format("Invalid ISBN number given: '%s'.", book.getIsbn()));
         }
 
@@ -45,15 +37,11 @@ public class MediaServiceImpl implements MediaService
     }
 
     @Override
-    public MediaServiceResult addDisc(Disc disc)
-    {
+    public MediaServiceResult addDisc(Disc disc) {
         final MediaServiceResult result;
-        if (mediaPersistence.getDiscIfExists(disc.getBarcode()) != null)
-        {
+        if (mediaPersistence.getDiscIfExists(disc.getBarcode()) != null) {
             result = new MediaServiceResult(Response.Status.BAD_REQUEST, String.format("Disc with Barcode '%s' already exists.", disc.getBarcode()));
-        }
-        else
-        {
+        } else {
             mediaPersistence.updateOrCreate(disc);
             result = new MediaServiceResult(Response.Status.CREATED, String.format("Disc with Barcode '%s' was successfully created.", disc.getBarcode()));
         }
@@ -61,45 +49,37 @@ public class MediaServiceImpl implements MediaService
     }
 
     @Override
-    public MediaServiceResult updateBook(Book book, String isbn)
-    {
+    public MediaServiceResult updateBook(Book book, String isbn) {
         final MediaServiceResult result;
 
         // Check Isbn
         final String checkedIsbn;
-        try
-        {
+        try {
             checkedIsbn = Isbn.of(isbn);
-        } catch (IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
             return new MediaServiceResult(Response.Status.BAD_REQUEST, String.format("The given ISBN (%s) was not valid", isbn));
         }
 
 
         final Book savedBook = mediaPersistence.getBookIfExists(checkedIsbn);
-        if (savedBook != null)
-        {
+        if (savedBook != null) {
             final String newTitle = book.getTitle().trim().isEmpty() ? savedBook.getTitle() : book.getTitle().trim();
             final String newAuthor = book.getAuthor().trim().isEmpty() ? savedBook.getAuthor() : book.getAuthor().trim();
 
             mediaPersistence.updateOrCreate(new Book(newTitle, newAuthor, checkedIsbn));
 
             result = new MediaServiceResult(Response.Status.OK, String.format("Book with ISBN '%s' was successfully updated.", book.getIsbn()));
-        }
-        else
-        {
+        } else {
             result = new MediaServiceResult(Response.Status.NOT_FOUND, String.format("Book with ISBN '%s' wasn't found.", book.getIsbn()));
         }
         return result;
     }
 
     @Override
-    public MediaServiceResult updateDisc(Disc disc)
-    {
+    public MediaServiceResult updateDisc(Disc disc) {
         final MediaServiceResult result;
         final Disc savedDisc = mediaPersistence.getDiscIfExists(disc.getBarcode());
-        if (savedDisc != null)
-        {
+        if (savedDisc != null) {
 
             final String newTitle = disc.getTitle().trim().isEmpty() ? savedDisc.getTitle() : disc.getTitle().trim();
             final String newDirector = disc.getDirector().trim().isEmpty() ? savedDisc.getDirector() : disc.getDirector().trim();
@@ -108,42 +88,34 @@ public class MediaServiceImpl implements MediaService
             mediaPersistence.updateOrCreate(new Disc(newTitle, disc.getBarcode(), newDirector, newFsk));
 
             result = new MediaServiceResult(Response.Status.OK, String.format("Disc with Barcode '%s' was successfully updated.", disc.getBarcode()));
-        }
-        else
-        {
+        } else {
             result = new MediaServiceResult(Response.Status.NOT_FOUND, String.format("Disc with Barcode '%s' wasn't found.", disc.getBarcode()));
         }
         return result;
     }
 
     @Override
-    public Book[] getBooks()
-    {
+    public Book[] getBooks() {
         return mediaPersistence.getBooks();
     }
 
     @Override
-    public Book getBook(String isbn)
-    {
-        try
-        {
+    public Book getBook(String isbn) {
+        try {
             final String checkedIsbn = Isbn.of(isbn);
             return mediaPersistence.getBookIfExists(checkedIsbn);
-        } catch (IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
             return null;
         }
     }
 
     @Override
-    public Disc[] getDiscs()
-    {
+    public Disc[] getDiscs() {
         return mediaPersistence.getDiscs();
     }
 
     @Override
-    public Disc getDisc(String barcode)
-    {
+    public Disc getDisc(String barcode) {
         return mediaPersistence.getDiscIfExists(barcode);
     }
 }
